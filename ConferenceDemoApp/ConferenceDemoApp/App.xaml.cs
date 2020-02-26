f3eff1b00c0d3d597f2660d69a332dce6f394ed4
André Krämer <andre.kraemer@qualitybytes.de>
2020-02-26 13:58:05 +0100

    Added Shell

--- ConferenceDemoApp/ConferenceDemoApp/App.xaml.cs
+++ ConferenceDemoApp/ConferenceDemoApp/App.xaml.cs
@@ -10,7 +10,7 @@ namespace ConferenceDemoApp
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new AppShell();
         }
 
         protected override void OnStart()
